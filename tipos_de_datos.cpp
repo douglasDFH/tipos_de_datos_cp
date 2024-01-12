b5c4a162596c520b65830a6eb8cd2c41b5d2607d
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-11 21:52:14 -0400

    tipos de datos

---
+++ tipos_de_datos.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	short int numero_entero_p;   
+	unsigned short int numero_entero_p1;
+	unsigned int numero_entero_s;
+	int numero_entero;
+	float numero_decimal;
+	double Numero_decimal_g;
+	
+	//asignando valores de limites de datos
+	numero_entero_p = -32767; // valor limite del tipo de dato
+	cout <<"numero entero pequeño: "<<numero_entero_p<<endl;//impresion de la variable numero_entero con un salto de linea
+	numero_entero_p1 = 6535; // valor limite del tipo de dato 6535 35560
+	cout <<"numero entero pequeño positivo: "<<numero_entero_p1<<endl;// impresion de la variable  numero_entero_p1 con salto de linea
+	numero_entero = ;32147483647 // valor limite del tipo de dato
+	cout <<"numero entero pequeño: "<<numero_entero<<endl;//impresion de la variable numero_entero con un salto de linea
+	numero_entero_s = 4294967295; // valor limite del tipo de dato4294967295 
+	cout <<"numero entero  positivo: "<<numero_entero_s<<endl;// impresion de la variable  numero_entero_s con salto de linea
+	// asignacion entre variables
+	numero_entero = numero_entero_p ; //no es correcto Los valores 
+	cuot<< "numero entreo pequeño 1 ra asignacion "<< numero_entero_p1<<end;
+	// rango de dtos short int -32768 al 32767
+	numero_entero_p = 32769 el valor esta fuera delrango de valores permitidos
+		numero_entero_p1= numero_entero_p ; //no es correcto Los valores 
+	cuot<< "numero entreo pequeño 2da  asignacion "<< numero_entero_p1<<end;
+	numero_entero_p= 32760// si es correcto los valores estan dentro del rango
+
+
+	
+	
+	
+	
+	return 0;
+}
